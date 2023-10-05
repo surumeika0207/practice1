860ba1f571dfd34e82d3332c2a31f2758c834a51
fujiwarakanato <K.fujiwara.sys23@morijyobi.ac.jp>
2023-10-05 14:31:15 +0900

    pra2.javaの内容
    Hello Worldを100回出力します。

--- pra2.java
+++ pra2.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
-    
-   
-public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello, World!");
+        }
     }
 }
